1984b5d6abe4b4e251f932513b5724d594af241a
amiralinmp841-alt <amiralinmp841@gmail.com>
2026-09-15 22:07:01 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1292,6 +1292,8 @@ if __name__ == "__main__":
     tg_app.add_handler(
         CallbackQueryHandler(callback_handler)
     )
+    
+    tg_app.add_handler(MessageHandler(filters.VOICE, handle_voice))
 
     # ---------------------------------------------
     # بررسی تنظیمات ضروری
